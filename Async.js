d97f81217ad90a0b1dc0c197ba95743bb431f928
Himanshu <gauravnegi149@gmail.com>
2022-06-20 00:20:46 +0530

    asyn

---
+++ Async.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+async function myFn() {
+    console.log("Before");
+    let promise = fs.promises.readFile("f1.txt");
+    console.log("Promise before", promise);
+    console.log("After");
+    // it stops the myfn function till promise is fullfilled
+    // and return value of that promise
+    let value = await promise;
+    console.log(value + "");
+}
+myFn();
